d89a6f58a2afa435cc877045a13cedfb061b0cb9
Max Stevenson <maxstevenson@msn.com>
2018-12-10 13:54:12 +0000

    - Refactored existing code for testing to utilise check method. - Implemented color scheme within test results to aid distinguishing outcomes. - Refactored check method to use color scheme.

--- playground/tests/string-utils.test.js
+++ playground/tests/string-utils.test.js
@@ -1,24 +1,28 @@
-const { guarantee } = require('tyrion');
+const { guarantee, check } = require('tyrion');
 const stringUtils = require('../src/string-utils');
 
-// Group: capitalizeFirstLetter method
-// Test: capitalizes the first letter of a string
-guarantee(stringUtils.capitalizeFirstLetter('why so serious?') === 'Why so serious?');
-// Test: don't change the case of the remaining characters
-guarantee(stringUtils.capitalizeFirstLetter('why So Serious?') === 'Why So Serious?');
+check('capitalizes the first letter of a string', () => {
+	guarantee(stringUtils.capitalizeFirstLetter('why so serious?') === 'Why so serious?');
+});
 
-// Group: isBlank method
-// Test: returns true when passing an empty string
-guarantee(stringUtils.isBlank(''));
-// Test: returns true when passing a string containing only spaces
-guarantee(stringUtils.isBlank('      '));
-// Test: returns false when passing a non-blank string
-guarantee(!stringUtils.isBlank('whatever'));
+check('don\'t change the case of the remaining characters', () => {
+	guarantee(stringUtils.capitalizeFirstLetter('why So Serious?') === 'Why So Serious?');
+});
 
-// Group: startsWith method
-// Test: returns true when it starts with the passed string
-guarantee(stringUtils.startsWith('Just when I...', 'Just'));
-// Test: returns false when it contains the passed string, but doesn't start with it
-guarantee(!stringUtils.startsWith('Just when I...', 'when I'));
-// Test: returns false when it doesn't contain the passed string
-guarantee(!stringUtils.startsWith('Just when I...', 'corleone'));
+check('returns true when passing an empty string', () => {
+	guarantee(stringUtils.isBlank(''));
+	guarantee(stringUtils.isBlank('      '));
+	guarantee(!stringUtils.isBlank('whatever'));
+});
+
+check('returns true when it starts with the passed string', () => {
+	guarantee(stringUtils.startsWith('Just when I...', 'Just'));
+});
+
+check('returns false when it contains the passed string, but doesn\'t start with it', () => {
+	guarantee(!stringUtils.startsWith('Just when I...', 'when I'));
+});
+
+check('returns false when it doesn\'t contain the passed string', () => {
+	guarantee(!stringUtils.startsWith('Just when I...', 'corleone'));
+});
